7f14c179362814f46ae31d70d36a1d13d90fabd5
Darshno <darshno18@gmail.com>
2026-08-23 02:08:16 +0530

    Enhance CycleLog schema with validation and uniqueness
    Updated cycleLogSchema to enforce required userId and added validation for cycleLength and periodLength.

--- server/models/CycleLog.js
+++ server/models/CycleLog.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const cycleLogSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
   lastPeriodStart: String,
-  cycleLength: { type: Number, default: 28 },
-  periodLength: { type: Number, default: 5 },
-});
+  cycleLength: { type: Number, default: 28, min: 21, max: 45 },
+  periodLength: { type: Number, default: 5, min: 1, max: 10 },
+}, { timestamps: true });
 
 export default mongoose.model("CycleLog", cycleLogSchema);

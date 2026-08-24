8507217b7fe56a5726adfa5d639237899c5e7534
Darshno <darshno18@gmail.com>
2026-08-24 10:00:53 +0530

    Wrap App component with LanguageProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
+import { LanguageProvider } from "./context/LanguageContext";
 import "./index.css";
 import "./styles/companion.css";
 import "./styles/layout.css";
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <ToastProvider>
-        <App />
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
       </ToastProvider>
     </AuthProvider>
   </React.StrictMode>
